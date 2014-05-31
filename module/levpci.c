c1b8498fbdae437d9fd083bd8e7424360e683a62
Levente Kurusa <levex@linux.com>
2014-05-31 11:50:18 +0200

    module: convert to pr_* format
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- module/levpci.c
+++ module/levpci.c
@@ -1,8 +1,12 @@
+/* Author: Levente Kurusa <levex@linux.com> */
+
+#define pr_fmt(fmt) "lev: " fmt
+
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pci.h>
-
+#include <linux/printk.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Levente Kurusa <levex@linux.com>");
@@ -15,13 +19,13 @@ static const struct pci_device_id pcidevtbl[] = {
 
 };
 
-static int levpci_probe(struct pci_dev *pdev, const struct pci_device_id *ent) {
-
+static int levpci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
+{
 	void __iomem *mmio;
 	char *buf;
 	int len, i;
 
-	printk("lev: probed pci dev, trying read.\n");
+	pr_debug("probed pci dev, trying read.\n");
 
 	mmio = pci_iomap(pdev, 0, 0);
 	if (!mmio) {
@@ -30,24 +34,24 @@ static int levpci_probe(struct pci_dev *pdev, const struct pci_device_id *ent) {
 	}
 
 	len = ioread8(mmio + 1);
-	printk("lev: length of string: %d bytes\n", len);
+	pr_info("length of string: %d bytes\n", len);
 
 	buf = kmalloc(len + 1, GFP_KERNEL);
 	if (!buf) {
-		printk("lev: no memory...\n");
+		pr_err("no memory...\n");
 		return -ENOMEM;
 	}
 
 	for (i = 0; i < len; i++)
 		buf[i] = ioread8(mmio);
 
-	printk("lev: read this string: %s", buf);
+	pr_info("read this string: %s", buf);
 
 	return 0;
 }
 
 static void levpci_remove(struct pci_dev *pdev) {
-	printk("lev: unloaded device\n");
+	pr_debug("unloaded device\n");
 }
 
 static struct pci_driver lev_pci_driver = {
@@ -61,11 +65,11 @@ static struct pci_driver lev_pci_driver = {
 static int __init levpci_init(void)
 {
 	int rc;
-	printk(KERN_INFO "lev: pci device driver online.\n");
+	pr_debug("pci device driver online.\n");
 
 	rc = pci_register_driver(&lev_pci_driver);
 	if (rc) {
-		printk(KERN_EMERG "lev: failed to register driver.\n");
+		pr_err("failed to register driver.\n");
 		return rc;
 	}
 
@@ -74,7 +78,7 @@ static int __init levpci_init(void)
 
 static void __exit levpci_exit(void)
 {
-	printk(KERN_INFO "lev: driver unloaded :-(\n");
+	pr_debug("driver unloaded :-(\n");
 	return;
 }
 
